f8eb77cebd6c1f9c05a1e3c9d32d70c586b07ebb
Fredrik Ahlgren <fredrik@ahlgren.nu>
2020-01-28 14:53:23 -0800

    working sensor DHT22

---
+++ Pycom/temp-hum/main.py
@@ -0,0 +1,26 @@
+import time
+from machine import Pin
+from dth import DTH
+import _thread
+
+# Type 0 = dht11
+# Type 1 = dht22
+
+th = DTH(Pin('P23', mode=Pin.OPEN_DRAIN), 1)
+time.sleep(2)
+
+
+def send_env_data():
+    while True:
+        result = th.read()
+        while not result.is_valid():
+            time.sleep(.5)
+            result = th.read()
+        #pybytes.send_signal(1, result.temperature) # noqa F821
+        #pybytes.send_signal(2, result.humidity) # noqa F821
+        print('Temp:', result.temperature)
+        print('RH:', result.humidity)
+        time.sleep(10)
+
+
+_thread.start_new_thread(send_env_data, ())
